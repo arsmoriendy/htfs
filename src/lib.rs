f4b12dbe5a5ff41f7888b34b68904f813f5e9889
arsmoriendy <nugrahasteven@gmail.com>
2025-02-23 17:32:39 +0700

    feat: implement getattr for every inode

--- src/lib.rs
+++ src/lib.rs
@@ -23,40 +23,48 @@ impl Filesystem for TagFileSystem {
     }
 
     fn getattr(&mut self, _req: &Request<'_>, ino: u64, _fh: Option<u64>, reply: ReplyAttr) {
-        // inode 1 is the mountpoint directory itself
-        if ino == 1 {
-            return reply.attr(
-                &Duration::from_secs(1),
-                &FileAttr {
-                    ino: 1,
-                    nlink: 1,
-                    rdev: 0,
+        let attr = match ino {
+            1 => FileAttr {
+                ino: 1,
+                nlink: 1,
+                rdev: 0,
 
-                    // TODO: size related
-                    size: 0,
-                    blocks: 0,
+                // TODO: size related
+                size: 0,
+                blocks: 0,
 
-                    // TODO: time related
-                    atime: SystemTime::UNIX_EPOCH,
-                    mtime: SystemTime::UNIX_EPOCH,
-                    ctime: SystemTime::UNIX_EPOCH,
-                    crtime: SystemTime::UNIX_EPOCH,
+                // TODO: time related
+                atime: SystemTime::UNIX_EPOCH,
+                mtime: SystemTime::UNIX_EPOCH,
+                ctime: SystemTime::UNIX_EPOCH,
+                crtime: SystemTime::UNIX_EPOCH,
 
-                    kind: FileType::Directory,
+                kind: FileType::Directory,
 
-                    // TODO: permission related, sync with original dir mayhaps?
-                    perm: 0o755, // rwx r-x r-x
+                // TODO: permission related, sync with original dir mayhaps?
+                perm: 0o755, // rwx r-x r-x
 
-                    // TODO: user/group related
-                    uid: _req.uid(),
-                    gid: _req.gid(),
+                // TODO: user/group related
+                uid: _req.uid(),
+                gid: _req.gid(),
 
-                    // TODO: misc
-                    blksize: 0,
-                    flags: 0,
-                },
-            );
-        }
+                // TODO: misc
+                blksize: 0,
+                flags: 0,
+            },
+            _ => {
+                let row: FileAttrRow = task::block_on(
+                    query_as("SELECT * FROM file_attrs WHERE ino = ?")
+                        .bind(ino as i64)
+                        .fetch_one(self.pool.as_ref()),
+                )
+                .unwrap();
+
+                row.into()
+            }
+        };
+
+        reply.attr(&Duration::from_secs(1), &attr);
     }
 
     fn lookup(
