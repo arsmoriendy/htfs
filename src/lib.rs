c048d10887a083bd2af23919f3139980fc3d2916
arsmoriendy <nugrahasteven@gmail.com>
2025-03-16 16:04:35 +0700

    feat: has_access method

--- src/lib.rs
+++ src/lib.rs
@@ -52,6 +52,43 @@ impl TagFileSystem<'_> {
             Ok(_) => Ok(()),
         }
     }
+
+    async fn has_access(
+        &self,
+        f_uid: u32,
+        f_gid: u32,
+        f_perm: u16,
+        uid: u32,
+        gid: u32,
+        rwx: u16,
+    ) -> bool {
+        if uid == 0 {
+            return true;
+        }
+
+        if f_uid == uid {
+            if f_perm >> 6 & rwx == rwx {
+                return true;
+            }
+
+            return false;
+        }
+
+        if f_gid == gid {
+            if f_perm >> 3 & rwx == rwx {
+                return true;
+            }
+
+            return false;
+        };
+
+        // permission for others
+        if f_perm & rwx == rwx {
+            return true;
+        }
+
+        return false;
+    }
 }
 
 impl Filesystem for TagFileSystem<'_> {
